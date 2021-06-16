1661a36702e162ba509549841804e3caa2c5eca7
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-06-16 14:25:13 -0300

    alterações iniciais na página de login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { LoginPage } from './components/index'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     teste
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route exact path="/login" component={ LoginPage } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
