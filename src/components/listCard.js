743ec5310859a130b96d2fa24beb68baeaef2336
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-06-17 15:38:33 -0300

    componente listCard implementado

--- src/components/listCard.js
+++ src/components/listCard.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import GlobalContext from '../context/globalContext';
 
 function ListCard() {
   const { users } = useContext(GlobalContext);
-
+  console.log(users[0]);
   return(
     <section>
-    {users.map((user) => {
-      console.log(user.login);
-    })}
+    {users.map((user) =>
+      <p> { user.login } </p>
+    )}
     </section>
   )
 }
